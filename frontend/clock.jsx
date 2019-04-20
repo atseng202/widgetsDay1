290a0b6b85d0dae0938173dc9513fc6b188515ee
Alan Tseng <atseng202@gmail.com>
2019-04-19 21:24:35 -0700

    finish clock styling

--- frontend/clock.jsx
+++ frontend/clock.jsx
@@ -4,7 +4,6 @@ class Clock extends React.Component {
   constructor(props) {
     super(props);
     this.state = {time: new Date()};
-
     this.tick = this.tick.bind(this);
   }
 
@@ -22,12 +21,16 @@ class Clock extends React.Component {
 
   render() {
     const {time} = this.state;
+    const date = time.toString().split(' ').slice(0, 4).join(' ');
     return (
-      <div>
+      <div class="clock">
         <h1>This is a Clock</h1>
-        <p>Hour: {time.getHours()}</p>
-        <p>Minutes: {time.getMinutes()}</p>
-        <p>Seconds: {time.getSeconds()}</p>
+        <section class="clock_time">
+          <h2 class="clock_header">Time: </h2><p> {time.getHours()}:{time.getMinutes()}:{time.getSeconds()} PDT</p>
+        </section>
+        <section class="clock_date">
+          <h2 class="clock_header">Date: </h2><p> {date}</p>
+        </section>
       </div>
     );
   }
